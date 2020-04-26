fd04e99786e8231a305e1dcc162826300662db5d
Yudha Prasetya Usman <yudhaprasetyausman@gmail.com>
2020-04-26 17:03:59 +0700

    add modal

--- proyek-medan/edit/edit-status.php
+++ proyek-medan/edit/edit-status.php
@@ -79,6 +79,26 @@ if ($result->num_rows > 0) {
 
     </tbody>
  </table>
+
+ <!-- The Modal -->
+<div id="myModal" class="modal">
+
+<!-- Modal content -->
+<div class="modal-content">
+  <div class="modal-header">
+    <span class="close">&times;</span>
+    <h2>Modal Header</h2>
+  </div>
+  <div class="modal-body">
+    <p>Some text in the Modal Body</p>
+    <p>Some other text...</p>
+  </div>
+  <div class="modal-footer">
+    <h3>Modal Footer</h3>
+  </div>
+</div>
+
+</div>
  <script src="/proyek-medan/javascript/modal.js"></script>
 </body>
 </html>
