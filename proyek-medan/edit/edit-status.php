61000c475f4767822265fe51197f0d48b0f5766a
Yudha Prasetya Usman <yudhaprasetyausman@gmail.com>
2020-04-26 14:44:56 +0700

    add phpinfo as test

--- proyek-medan/edit/edit-status.php
+++ proyek-medan/edit/edit-status.php
@@ -6,6 +6,24 @@
     <title>Daftar Nama Tukang</title>
 </head>
 <body>
+<table>
+     <caption>
+         Daftar Nama Tukang
+     </caption>
+
+     <thead>
+         <tr>
+             <th>No</th>
+             <th>No ID</th>
+             <th>Nama</th>
+             <th>Jabatan</th>
+             <th>Gaji/Hari</th>
+             <th>Tanggal Masuk</th>
+             <th>Alamat</th>
+             <th>Status</th>
+         </tr>
+     </thead>
+     <tbody>
 
 <!-- Sql Select for PHP -->
 
@@ -22,40 +40,31 @@ if ($result->num_rows > 0) {
         $date = strtotime($row['tgl_masuk']);
         $tgl_format = date("d F Y", $date);
         $rp_upah = number_format(floatval($row['upah']));
-    }
-}
-$conn->close();
-?>
 
- <table>
-     <caption>
-         Daftar Nama Tukang
-     </caption>
-
-     <thead>
+        echo "
          <tr>
-             <th>No</th>
-             <th>Nama</th>
-             <th>Jabatan</th>
-             <th>Gaji/Hari</th>
-             <th>Tanggal Masuk</th>
-             <th>Alamat</th>
-             <th>Status</th>
-         </tr>
-     </thead>
-     <tbody>
-         <tr>
-             <td><?php echo $number ?></td>
-             <td><?php echo $row["nama_karyawan"] ?></td>
-             <td><?php echo $row["jabatan"] ?></td>
-             <td><?php echo $row["upah"] ?></td>
-             <td><?php echo $tgl_format ?></td>
+             <td>".$number."</td>
+             <td>".$row["id"]."</td>
+             <td>".$row["nama_karyawan"]."</td>
+             <td>".$row["jabatan"]."</td>
+             <td>Rp ".$rp_upah."</td>
+             <td>"$tgl_format"</td>
              <td></td>
-             <td><?php echo $row["status"] ?></td>
-         </tr>
-     </tbody>
+             <td>".$row["status"]."</td>
+         </tr>";
+         $number++;
+        }
+    } else {
+        # code...
+        echo "
+        <tr>
+        <td>Tidak Ada Tukang</td>
+        </tr>";
+    }
+    $conn->close();
+    ?>
+
+    </tbody>
  </table>
-<?php $number++; ?>
- 
 </body>
 </html>
