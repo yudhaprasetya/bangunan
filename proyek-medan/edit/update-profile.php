57b926ddb67f15c50c61c9ffa19378968fbaceed
Yudha Prasetya Usman <yudhaprasetyausman@gmail.com>
2020-04-27 13:37:56 +0700

    add change profile

--- proyek-medan/edit/update-profile.php
+++ proyek-medan/edit/update-profile.php
@@ -10,7 +10,7 @@ if (isset($_POST["change"])) {
     $tgl_indo = date("Y-m-d, $tanggal");
     $status_aktif = $_POST['status_aktif'];
 
-  $sql = "UPDATE tukang SET nama_karyawan = '$nama', jabatan = '$jabatan', upah = '$gaji', tgl_masuk = '$tanggal', status_aktif = '$status_aktif' WHERE id='$id'";
+  $sql = "UPDATE tukang SET nama_karyawan = '$nama', jabatan = '$jabatan', upah = '$gaji', tgl_masuk = '$tgl_indo', status_aktif = '$status_aktif' WHERE id='$id'";
   mysqli_select_db($conn, $sql);
   $retval = mysqli_query($conn, $sql);
 
