6fe0b78150529224e125ef55cf27123f70d1bce7
Yudha Prasetya Usman <yudhaprasetyausman@gmail.com>
2020-04-28 18:13:20 +0700

    add css for table

--- proyek-medan/index.php
+++ proyek-medan/index.php
@@ -1,3 +1,10 @@
-<?php
-$date = date('now');
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    
+  </body>
+</html>
