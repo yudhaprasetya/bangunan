e7da861dfa3f60d8767e1698897b40e6603d26d1
Yudha Prasetya Usman <yudhaprasetyausman@gmail.com>
2020-04-28 21:54:36 +0700

    add link to gaji

--- proyek-medan/index.php
+++ proyek-medan/index.php
@@ -2,9 +2,25 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyek Bangunan - Medan</title>
+    <link rel="stylesheet" type="text/css" href="/proyek-medan/style/master.css">
   </head>
   <body>
-    
+
+    <?php
+    date_default_timezone_set("Asia/Jakarta");
+    $now = date("Y-m-d");
+    $tanggal_str = strtotime("Saturday");
+    $sabtu = date("Y-m-d", $tanggal_str);
+    ?>
+
+    <div class="center">
+
+    </div>
+
+    <a href="gaji/absen.php?<?php echo "$sabtu"; ?>">Absensi Gaji</a>
+    <a href="absen/all-profile.php">Daftar Tukang Bangunan</a>
+
   </body>
 </html>
