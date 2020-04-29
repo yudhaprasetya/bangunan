bbf6d19aa7c439cecb2dbd1ced874c24a3f9f70d
Yudha Prasetya Usman <yudhaprasetyausman@gmail.com>
2020-04-29 15:34:24 +0700

    change tfoot

--- proyek-medan/gaji/absen.php
+++ proyek-medan/gaji/absen.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title></title>
-  <style media="screen">
+  <style media="screen, print">
   <?php
   include '../style/table-def.css';
   include '../style/master.css';
