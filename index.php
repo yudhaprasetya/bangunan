f85c1c646bd0dcc648729db3ba8aca29deb81053
Yudha Prasetya Usman <yudhaprasetyausman@gmail.com>
2020-05-14 17:11:54 +0700

    add form login

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])) {
+  header('location:/users/');
+} else {
+  $username = $_SESSION['username'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CODELAB: Add iOS meta tags and icons -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="Weather PWA">
-    <meta name="description" content="A Simple Weather App">
-    <meta name="theme-color" content="2F3BA2">
-    <link rel="apple-touch-icon" href="/images/icons/icon-152x152.png">
-    <title>Proyek Medan</title>
-    <link rel="manifest" href="/manifest.json">
-    <link rel="stylesheet" href="/style/style.css">
-    <link rel="stylesheet" href="/proyek-medan/style/table.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Proyek Bangunan</title>
+
+  <style media="screen">
+    <?php include 'style/master.css'; ?>
+  </style>
 </head>
 <body>
-    <h1>Document Infinity Is Ready</h1>
-    <h2>Update 2</h2>
-    <a href="/proyek-medan/">Proyek Bangunan Gg Kasih - Medan</a>
 
+  <header>
+    <p>Proyek Bangunan</p>
+  </header>
 
-    <script src="/scripts/install.js"></script>
 </body>
 </html>
