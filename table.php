13518f49ac4a5b048d591d3093d86db88d01e553
Yudha Prasetya Usman <yudhaprasetyausman@gmail.com>
2020-05-21 00:24:20 +0700

    FGD

---
+++ table.php
@@ -0,0 +1,30 @@
+<style media="screen">
+body {
+  margin: 0;
+}
+  table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+  }
+</style>
+
+<?php
+
+if ($a = ) {
+  // code...
+} ?>
+<table>
+  <tr>
+    <td style="display: none">asdasd</td>
+    <td>asdadas</td>
+    <td style="display: none">asdasd</td>
+    <td>asdadas</td>
+  </tr>
+  <tr>
+    <td style="display: none">asdasd</td>
+    <td>asdadas</td>
+    <td style="display: none">asdasd</td>
+    <td>asdadas</td>
+  </tr>
+
+</table>
